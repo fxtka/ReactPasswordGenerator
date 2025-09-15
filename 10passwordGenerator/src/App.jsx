582c6eba02456411e6dcc3a33338a89717f4120d
Fatika Azam <fatikahazam@gmail.com>
2025-09-15 18:38:49 +0500

    generator func + frontend

--- 10passwordGenerator/src/App.jsx
+++ 10passwordGenerator/src/App.jsx
@@ -13,16 +13,84 @@ function App() {
     if (numAllowed) str+='0123456789'
     if (charAllowed) str+='`~!@#$%^&*_+={}[]'
 
-
-  }, [length, numAllowed, charAllowed, setPassword])
-  for (let i = 1; i <= array.leng; i++) {
+    for (let i = 1; i <= array.leng; i++) {
     let char =Math.floor(Math.random() * str.length +1)
+    pass = str.charAt(char)
     
   }
+  setPassword(pass)
+  
+
+
+  }, [length, numAllowed, charAllowed, setPassword])
+  
+  
 
   return (
     <>
-      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-800'>
+
+        <h1 className='text-white text center my-3'>Password Generator</h1>
+        
+        <div className='flex-shadow rounded-lg overflow-hidden mb-4 bg-white'>
+          <input type="text" 
+          value={password}
+          className='outline-none w-full py-1 px-3'
+          placeholder='Password'
+          readOnly
+          />
+
+          <button className='outline-none bg-blue-700 text-white px-3 py-0.5'>
+            Copy
+            </button>
+
+        </div>
+
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+             <input
+             type="range"
+             min={8}
+             max={100}
+             value={length}
+             className='cursor-pointer'
+             onChange={(e)=>{setLength(e.target.value)}}
+              />
+              <label >Length: {length}</label>
+          </div> 
+
+          <div className='flex items-center gap-x-1'>
+            <input
+            type="checkbox"
+            defaultChecked= {numAllowed}
+            id="numInput" 
+            onChange={()=>{
+              setNumAllowed((prev) => !prev);
+            }}
+            />
+            <label >Numbers</label>
+
+          </div>
+
+          <div className='flex items-center gap-x-1'>
+            <input
+            type="checkbox"
+            defaultChecked= {charAllowed}
+            id="charInput" 
+            onChange={()=>{
+              setCharAllowed((prev) => !prev);
+            }}
+            />
+            <label >Characters</label>
+
+          </div>
+
+
+        </div>
+        
+
+      </div>
+
     </>
   )
 }
